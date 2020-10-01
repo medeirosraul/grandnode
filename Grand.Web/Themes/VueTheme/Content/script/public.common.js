2df3cbeb99915aec46c6807e91163817912ccf20
Daniel Żuławski <dzulawski@unit-soft.pl>
2020-10-01 10:55:02 +0200

    Vue Theme - flyout cart rendering

---
+++ Grand.Web/Themes/VueTheme/Content/script/public.common.js
@@ -0,0 +1,32 @@
+﻿
+function deletecartitem(href) {
+    var flyoutcartselector = AxiosCart.flyoutcartselector;
+    var topcartselector = AxiosCart.topcartselector;
+    axios({
+        method: "post",
+        url: href
+    }).then(function (response) {
+        var flyoutcart = response.data.flyoutshoppingcart;
+        new Vue({
+            el: '.flyout-cart',
+            data: {
+                template: null
+            },
+            render: function (createElement) {
+                if (!this.template) {
+                    
+                } else {
+                    return this.template();
+                }
+            },
+            mounted() {
+                var self = this;
+                self.template = Vue.compile(flyoutcart).render;
+            }
+        });
+        document.querySelector(topcartselector).innerHTML = response.data.totalproducts;
+    }).catch(function (xhr, ajaxOptions, thrownError) {
+        console.log('Failed to retrieve Flyout Shopping Cart.');
+    });
+    return false;
+}
