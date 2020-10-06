c0f49917ef5d11fe94bdf9f59bcb355484f8d1cc
Daniel Żuławski <dzulawski@unit-soft.pl>
2020-10-06 14:48:51 +0200

    Vue Theme -  shopping cart - submit buttons

---
+++ Grand.Web/Themes/VueTheme/Content/script/cart.js
@@ -0,0 +1,28 @@
+﻿function deleteitem(href) {
+    axios({
+        method: 'post',
+        url: href,
+    }).then(function (response) {
+        var flyoutcart = response.data.flyoutshoppingcart;
+        new Vue({
+            el: '.flyout-cart',
+            data: {
+                template: null
+            },
+            render: function (createElement) {
+                if (!this.template) {
+
+                } else {
+                    return this.template();
+                }
+            },
+            mounted() {
+                var self = this;
+                self.template = Vue.compile(flyoutcart).render;
+            }
+        });
+        window.location.reload();
+    }).catch(function (error) {
+        alert('Failed to retrieve Shopping Cart Page.');
+    })
+}
