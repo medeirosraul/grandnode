7dc4bc529e17df8d7c1b82666561b4be370942fe
Daniel Żuławski <dzulawski@unit-soft.pl>
2020-10-07 11:14:54 +0200

    Vue Theme - cart - minor changes

--- Grand.Web/Themes/VueTheme/Content/script/cart.js
+++ Grand.Web/Themes/VueTheme/Content/script/cart.js
@@ -26,3 +26,71 @@
         alert('Failed to retrieve Shopping Cart Page.');
     })
 }
+
+function ChangeShoppingCart() {
+    var form = document.getElementById('shopping-cart-form');
+    var data = new FormData(form);
+    axios({
+        method: 'post',
+        data: data,
+        url: '/updatecart',
+    }).then(function (response) {
+        var cartChange = response.data.cart;
+        //new Vue({
+        //    el: '.static-cart',
+        //    data: {
+        //        template: null
+        //    },
+        //    render: function (createElement) {
+        //        if (!this.template) {
+
+        //        } else {
+        //            return this.template();
+        //        }
+        //    },
+        //    mounted() {
+        //        var self = this;
+        //        self.template = Vue.compile(cartChange).render;
+        //    }
+        //});
+        window.location.reload();
+    }).catch(function (error) {
+        alert('Failed to retrieve Shopping Cart Page.');
+    })
+}
+//setTimeout(function () {
+//    var button = document.querySelectorAll(".changeshoppingcartitem");
+//    button.forEach(function () {
+//        this.addEventListener("click", function (e) {
+//            var self = this;
+//            var href = self.querySelectorAll('.custom-control-input').getAttribute('data-href');
+//            axios({
+//                method: 'post',
+//                data: data,
+//                url: href,
+//            }).then(function (response) {
+//                var cartChange = response.data.cart;
+//                //new Vue({
+//                //    el: '.static-cart',
+//                //    data: {
+//                //        template: null
+//                //    },
+//                //    render: function (createElement) {
+//                //        if (!this.template) {
+
+//                //        } else {
+//                //            return this.template();
+//                //        }
+//                //    },
+//                //    mounted() {
+//                //        var self = this;
+//                //        self.template = Vue.compile(cartChange).render;
+//                //    }
+//                //});
+//                window.location.reload();
+//            }).catch(function (error) {
+//                alert('Failed to retrieve Shopping Cart Page.');
+//            })
+//        }, false);
+//    });
+//}, 300);
