f19e8f57ed7410916408113048616f3c473274eb
Daniel Żuławski <dzulawski@unit-soft.pl>
2020-10-15 14:54:52 +0200

    Vue Theme - script inside modal quickview fixed

--- Grand.Web/Themes/VueTheme/Content/script/public.common.js
+++ Grand.Web/Themes/VueTheme/Content/script/public.common.js
@@ -58,6 +58,7 @@ function displayPopupAddToCart(html) {
 }
 
 function displayPopupQuickView(html) {
+    document.querySelector('.modal-place').innerHTML = html;
     new Vue({
         el: '#ModalQuickView',
         data: {
@@ -75,6 +76,9 @@ function displayPopupQuickView(html) {
         methods: {
             showModal() {
                 this.$refs['ModalQuickView'].show()
+            },
+            onShow() {
+                setTimeout(function () { runScripts(document.querySelector('.script-tag')) }, 300);
             }
         },
         mounted() {
@@ -183,4 +187,97 @@ window.onload = function () {
             document.getElementById("newsletter-subscribe-button").click();
         }
     });
+}
+
+// runs an array of async functions in sequential order
+function seq(arr, callback, index) {
+    // first call, without an index
+    if (typeof index === 'undefined') {
+        index = 0
+    }
+
+    arr[index](function () {
+        index++
+        if (index === arr.length) {
+            callback()
+        } else {
+            seq(arr, callback, index)
+        }
+    })
+}
+
+// trigger DOMContentLoaded
+function scriptsDone() {
+    var DOMContentLoadedEvent = document.createEvent('Event')
+    DOMContentLoadedEvent.initEvent('DOMContentLoaded', true, true)
+    document.dispatchEvent(DOMContentLoadedEvent)
+}
+
+/* script runner
+ */
+
+function insertScript($script, callback) {
+    var s = document.createElement('script')
+    s.type = 'text/javascript'
+    if ($script.src) {
+        s.onload = callback
+        s.onerror = callback
+        s.src = $script.src
+    } else {
+        s.textContent = $script.innerText
+    }
+
+    // re-insert the script tag so it executes.
+    document.body.appendChild(s)
+
+    // clean-up
+    $script.parentNode.removeChild($script)
+
+    // run the callback immediately for inline scripts
+    if (!$script.src) {
+        callback()
+    }
+}
+
+// https://html.spec.whatwg.org/multipage/scripting.html
+var runScriptTypes = [
+    'application/javascript',
+    'application/ecmascript',
+    'application/x-ecmascript',
+    'application/x-javascript',
+    'text/ecmascript',
+    'text/javascript',
+    'text/javascript1.0',
+    'text/javascript1.1',
+    'text/javascript1.2',
+    'text/javascript1.3',
+    'text/javascript1.4',
+    'text/javascript1.5',
+    'text/jscript',
+    'text/livescript',
+    'text/x-ecmascript',
+    'text/x-javascript'
+]
+
+function runScripts($container) {
+    // get scripts tags from a node
+    var $scripts = $container.querySelectorAll('script')
+    var runList = []
+    var typeAttr
+
+    [].forEach.call($scripts, function ($script) {
+        typeAttr = $script.getAttribute('type')
+
+        // only run script tags without the type attribute
+        // or with a javascript mime attribute value
+        if (!typeAttr || runScriptTypes.indexOf(typeAttr) !== -1) {
+            runList.push(function (callback) {
+                insertScript($script, callback)
+            })
+        }
+    })
+
+    // insert the script tags sequentially
+    // to preserve execution order
+    seq(runList, scriptsDone)
 }
