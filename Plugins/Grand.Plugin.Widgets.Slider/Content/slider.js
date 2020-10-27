b75940c59c663319731ac915698a91c10e43838e
Daniel Żuławski <dzulawski@unit-soft.pl>
2020-10-27 14:10:09 +0100

    Bootstrap slider - minor changes

--- Plugins/Grand.Plugin.Widgets.Slider/Content/slider.js
+++ Plugins/Grand.Plugin.Widgets.Slider/Content/slider.js
@@ -1,39 +1,9 @@
-﻿//$(document).ready(function () {
-//    SliderPosition();
-//    $("#GrandCarousel .carousel-indicators:first, #GrandCarousel .carousel-inner .carousel-item:first").addClass("active");
-//    $("#GrandCarousel .carousel-indicators li").each(function () {
-//        var car_ind = $(this).index();
-//        $(this).attr("data-slide-to", car_ind);
-//    });
-//});
-//$(window).resize(function () {
-//    SliderPosition();
-//});
-//$("#next").click(function () {
-//    $('#grandCarousel').carousel('next');
-//});
+﻿// fire if first slide has set full width
 
-//$("#prev").click(function () {
-//    $('#grandCarousel').carousel('prev');
-//});
+if (document.querySelector('#GrandCarousel .carousel-item').getAttribute('data-width') == 'full') {
+    var mainContainer = document.querySelector(".main-container");
+    var GrandCarousel = document.getElementById("GrandCarousel");
+    var parentDiv = mainContainer.parentNode;
 
-//$(".carousel").on("touchstart", function (event) {
-//    var xClick = event.originalEvent.touches[0].pageX;
-//    $(this).one("touchmove", function (event) {
-//        var xMove = event.originalEvent.touches[0].pageX;
-//        if (Math.floor(xClick - xMove) > 5) {
-//            $(".carousel").carousel('next');
-//        }
-//        else if (Math.floor(xClick - xMove) < -5) {
-//            $(".carousel").carousel('prev');
-//        }
-//    });
-//    $(".carousel").on("touchend", function () {
-//        $(this).off("touchmove");
-//    });
-//});
-
-//function SliderPosition() {
-//    var FixSliderPos = $('.custom-container').position().left + 'px';
-//    $('#GrandCarousel').css('left', '-' + FixSliderPos);
-//}
+    parentDiv.insertBefore(GrandCarousel, mainContainer);
+}
