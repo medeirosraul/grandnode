a9eae247d399ee969c6558adb7f512e3c7e9e123
Daniel Żuławski <dzulawski@unit-soft.pl>
2020-10-28 11:42:13 +0100

    bootstrap slider - minor changes

--- Plugins/Grand.Plugin.Widgets.Slider/Content/slider.js
+++ Plugins/Grand.Plugin.Widgets.Slider/Content/slider.js
@@ -1,9 +1,11 @@
-﻿// fire if first slide has set full width
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // fire if first slide has set full width
 
-if (document.querySelector('#GrandCarousel .carousel-item').getAttribute('data-width') == 'full') {
-    var mainContainer = document.querySelector(".main-container");
-    var GrandCarousel = document.getElementById("GrandCarousel");
-    var parentDiv = mainContainer.parentNode;
+    if (document.querySelector('#GrandCarousel .carousel-item').getAttribute('data-width') == 'full') {
+        var mainContainer = document.querySelector(".main-container");
+        var GrandCarousel = document.getElementById("GrandCarousel");
+        var parentDiv = mainContainer.parentNode;
 
-    parentDiv.insertBefore(GrandCarousel, mainContainer);
-}
+        parentDiv.insertBefore(GrandCarousel, mainContainer);
+    }
+});
