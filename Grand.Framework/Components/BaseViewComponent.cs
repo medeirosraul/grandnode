abeedaa95de71e524d9894940f89686d2761aed9
KrzysztofPajak <krzysiek@nop4you.com>
2020-12-06 11:30:05 +0100

    Add component controller

--- Grand.Framework/Components/BaseViewComponent.cs
+++ Grand.Framework/Components/BaseViewComponent.cs
@@ -1,21 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewComponents;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Grand.Framework.Components
 {
+    [BaseViewComponent(AdminAccess = false)]
     public abstract class BaseViewComponent : ViewComponent
     {
-        public new ViewViewComponentResult View<TModel>(string viewName, TModel model)
+        public new IViewComponentResult View<TModel>(string viewName, TModel model)
         {
             return base.View<TModel>(viewName, model);
         }
 
-        public new ViewViewComponentResult View<TModel>(TModel model)
+        public new IViewComponentResult View<TModel>(TModel model)
         {
+            var viewJson = Request?.Headers["X-Response-View"];
+            if (viewJson?.Equals("Json") ?? false)
+            {
+                return new JsonContentViewComponentResult(new HtmlString(JsonConvert.SerializeObject(model)));
+            }
             return base.View<TModel>(model);
         }
 
-        public new ViewViewComponentResult View(string viewName)
+        public new IViewComponentResult View(string viewName)
         {
             return base.View(viewName);
         }
